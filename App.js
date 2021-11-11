eeabbe1759301c0f9438d0bf3d653e2d1f040bad
Tehmeenazafar <tehmeenazafar786@gmail.com>
2021-11-11 17:06:14 +0500

    Second commit

--- App.js
+++ App.js
@@ -8,12 +8,7 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
@@ -21,37 +16,9 @@ import { WebView } from 'react-native-webview';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 const App = () => {
-  
-
   return (
-    <SafeAreaView >
-      <View style={{
-        width:wp('100%'),
-        height:hp('100%')
-      }}>
-      <WebView source={{ uri: 'https://darkoi.labd.tech/darkoi/student/Login' }} />;
-      </View>
-    </SafeAreaView>
+    <WebView source={{ uri: 'https://darkoi.labd.tech/darkoi/student/login' }} />
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
